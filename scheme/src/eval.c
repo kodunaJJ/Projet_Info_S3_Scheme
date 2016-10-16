20d8225c0b23aadd3d6d078ff6a4c26bd6349d72
Quantuuum <clement.cheung35@gmail.com>
2016-10-16 21:53:40 +0200

    premier eval.c

--- scheme/src/eval.c
+++ scheme/src/eval.c
@@ -9,8 +9,40 @@
  */
 
 #include "eval.h"
+#include "object.h"
 
 object sfs_eval( object input ) {
+
+object output=input ;
+
+/* Gestion de QUOTE */
+if (output->type==SFS_SYMBOL && output->this.symbol[0]=='\''){ 
+	output=make_pair();
+	output->this.pair.car = make_symbol("quote") ;
+	int i =1;
+	while (input->this.symbol[i] != '\0'){ 
+		output->this.pair.cdr->this.symbol[i-1] = input->this.symbol[i];
+		i++;
+		}
+	return output ;
+	}
+
+if (output->type==SFS_PAIR && strcmp(output->this.pair.car->this.symbol , "quote")){
+	return output->this.pair.cdr;
+	}
+
+
+/*Gestion de DEFINE */
+if (output->type==SFS_PAIR && strcmp(output->this.pair.car->this.symbol , "define")){
+	}
+
+/*Gestion de set! */
+	
+	
+	
+
+else return output;
+
 
     return input;
 }
