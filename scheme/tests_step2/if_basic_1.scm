cff0d2d163fe1b7e1ce4cdd4829dce6c45b51459
jordan morra-taroudjit <m-t.jordan@live.fr>
2017-01-02 21:24:12 +0100

    ajout fichier test basic pour la form if

--- scheme/tests_step2/if_basic_1.scm
+++ scheme/tests_step2/if_basic_1.scm
@@ -1,6 +1,7 @@
 ; TEST_RETURN_CODE=PASS
-; TEST_COMMENT=Test eval quote tokens
+; TEST_COMMENT=Test eval if wihout alternative tokens
 
+;condition test with unsigned integer
 (if 1 2)
 (if 1 -2)
 (if 1 #t)
@@ -13,6 +14,15 @@
 (if 1 (quote ok))
 (if 1 (and 2 3))
 (if 1 (or 5 6))
+;condition test with signed integer
+(if -1 2)
+;condition test with character
+(if #\a 5)
+;condition test with string
+(if "abc" 6)
+;condition test with boolean 
+(if #t 7)
+(if #f 8)
 
 ;;; Make sure you put a return at
 ;;; the end of the input file
